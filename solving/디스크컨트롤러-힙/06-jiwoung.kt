bc7027f6dd6ab2e16a6b036d5b97fa77e5d3b3bc
choijw9691 <43192991+choijw9691@users.noreply.github.com>
2021-05-02 04:39:28 +0900

    Update 06-jiwoung.kt

--- solving/디스크컨트롤러-힙/06-jiwoung.kt
+++ solving/디스크컨트롤러-힙/06-jiwoung.kt
@@ -1,1 +1,38 @@
-..
+import java.util.*
+class Solution {
+    fun solution(jobs: Array<IntArray>): Int {
+        var answer = 0
+        var time = 0
+        var waittingQueue = PriorityQueue<Job>(compareBy({ it.start }))
+
+        for (i in 0..jobs.size - 1) {
+            var job = Job(jobs[i][0], jobs[i][1])
+            waittingQueue.offer(job)
+        }
+        while (!waittingQueue.isEmpty()) {
+            var readyQueue = PriorityQueue<Job>(compareBy { it.during })
+            while (!waittingQueue.isEmpty() && time >= waittingQueue.peek().start) {
+                readyQueue.offer(waittingQueue.poll())
+            }
+            if (readyQueue.isEmpty()) {
+                time++
+                continue
+            }
+            time += readyQueue.peek().during
+            answer += time - readyQueue.peek().start
+            readyQueue.poll()
+
+            for (i in 0..readyQueue.size - 1) {
+                waittingQueue.offer(readyQueue.poll())
+            }
+        }
+        answer /= jobs.size
+        return answer
+    }
+}
+
+data class
+Job(
+    var start: Int = 0,
+    var during: Int = 0
+)
