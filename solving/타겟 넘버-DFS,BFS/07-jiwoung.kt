4c6d9c53cafb7d3788bd38bd8e998e1fa87da709
choijw9691 <43192991+choijw9691@users.noreply.github.com>
2021-05-06 11:42:17 +0900

    Update 07-jiwoung.kt

--- solving/타겟 넘버-DFS,BFS/07-jiwoung.kt
+++ solving/타겟 넘버-DFS,BFS/07-jiwoung.kt
@@ -1,35 +1,22 @@
 class Solution {
+      fun solution(numbers: IntArray, target: Int): Int {
+        var answer = 0
+        answer = dfs(numbers, target, numbers[0], 1) + dfs(numbers, target, -numbers[0], 1);
 
-    var result = ArrayList<Int>()
-    var ta:Int=0
-    fun swap(array: IntArray, a: Int, b: Int) {
-        val temp = array[a]
-        array[a] = array[b]
-        array[b] = temp
+        return answer
     }
 
-    fun su(numList: IntArray, depth: Int, length: Int) {
-
-        if (depth == length) {
-            var sb :Int=0
-            for (j in 0 until length) {
-                numList[j]= (-1*numList[j])
-            }
-            for (i in 0..numList.size-1){
-                sb+=numList[i]
-            }
-            if (sb.toString().toInt()==ta){
-                result.add(sb.toString().toInt())
-            }
-
-            return
+    fun dfs(numbers: IntArray, target: Int, sum: Int, i: Int): Int {
+        var count = 0
+        if (i == numbers.size) {
+if (sum==target){
+    return 1
+}
+            else return 0
         }
+        count += dfs(numbers, target, sum + numbers[i], i + 1)
+        count += dfs(numbers, target, sum - numbers[i], i + 1)
 
-        for (i in depth..numList.size-1) {
-
-            swap(numList, depth, i)
-            su(numList, depth+1, length)
-            swap(numList, depth, i)
-        }
-
+        return count
     }
+}
